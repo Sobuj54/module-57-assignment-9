db094441862632bf41b97e776927d3ce81efc056
soubj <sobujahmed124@gmail.com>
2023-04-28 21:17:58 +0600

    completed section containing user image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import User from "../User/User";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <User></User>
     </div>
   );
 };
